ebcebff9fcee7ca401e449701729cf07808a7577
zhangqixuan <zhangqixuan@qq.com>
2021-09-08 14:25:15 +0800

    gin

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"zqx.com/goutil/middlewares"
 )
@@ -8,4 +10,8 @@ import (
 func main() {
 	r := gin.Default()
 	r.Use(middlewares.Cors())
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "Hello")
+	})
+	r.Run(":7777")
 }
